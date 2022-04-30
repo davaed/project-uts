5bb6eb988532e20a6007f716cdb408b489b8e87d
davaed <raplifikri589@gmail.com>
2022-04-30 16:06:34 +0700

    :sparkles: added user remove selected order for go-ride

--- components/ModalOrder.js
+++ components/ModalOrder.js
@@ -1,4 +1,14 @@
-export default function ModalOrder({ selectedCoordinates, setConfirmed }) {
+export default function ModalOrder({
+  selectedCoordinates,
+  setListCoordinates,
+  setConfirmed,
+}) {
+  function removeSelectedCoordinates(index) {
+    setListCoordinates(
+      selectedCoordinates.filter((_, point) => point !== index)
+    )
+  }
+
   return (
     <div className='fixed top-0 left-0 w-screen h-screen bg-stone-900 bg-opacity-10 px-5 sm:px-10 lg:px-0 z-[9999]'>
       <div className='h-full flex flex-col justify-center items-center'>
@@ -16,13 +26,14 @@ export default function ModalOrder({ selectedCoordinates, setConfirmed }) {
             <div
               key={index}
               className='grid grid-cols-1 sm:grid-cols-2 cursor-pointer border border-[#eaeaea] hover:border-[crimson] hover:text-[crimson] rounded-md p-2 px-3 m-4'
+              onClick={() => removeSelectedCoordinates(index)}
             >
               <div className='col-span-1 flex flex-col justify-start'>
                 <p>
                   <span className='font-semibold'>From:</span> Your Home
                 </p>
                 <p className='font-semibold'>Destination:</p>
-                <p className="space-x-1">
+                <p className='space-x-1'>
                   <span>{item.destination.address}</span>
                   <span className='italic'>Street/Place</span>
                 </p>
--- pages/go-ride/index.js
+++ pages/go-ride/index.js
@@ -37,6 +37,7 @@ export default function GoRide(props) {
         {confirmed && (
           <ModalOrder
             selectedCoordinates={listCoordinates}
+            setListCoordinates={setListCoordinates}
             setConfirmed={setConfirmed}
           />
         )}
